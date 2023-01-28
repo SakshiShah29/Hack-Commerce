e6f3a6789d2ec33c0928d8b3293d996c64c5ca2c
Sakshi Shah <sakshi.shah299@gmail.com>
2023-01-28 13:07:25 +0530

    Assets created

--- Frontend/components/NavBar.tsx
+++ Frontend/components/NavBar.tsx
@@ -70,14 +70,14 @@ const smallViewExtraNavigation = [
 export const NavBar = () => {
   const router = useRouter()
   return (
-    <Disclosure as="nav" className="bg-gray-700">
+    <Disclosure as="nav" className="bg-gradient-to-r from-violet-600 to-indigo-600">
       {({ open }) => (
         <div className="shadow-lg">
           <div className="mx-auto max-w-7xl px-2 sm:px-8 lg:px-10">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="relative inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-FFFAF1-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
